7aeb2f85855fdd51ece0314e39dc87bbf31a9154
Eduarjavis <geej5791@gmail.com>
2020-11-19 20:35:22 -0600

    Bus

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {HeroesService, Heroe} from '../../servicios/heroes.services'
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:Heroe[]=[]
+
+  constructor(
+    private _heroesService:HeroesService,
+    private _router:Router
+    ) {     
+  }
+
+  ngOnInit(): void {
+    this.heroes=this._heroesService.getHeroes();    
+  }
+  //esta es una forma de moverte por la ruta con la etiqueta boton
+  //se necesita importar Router de angular
+  verHeroe(idx:number){
+  this._router.navigate(['heroe',idx])//ruta y variable 
+  }
+  
+}
